71a0bbed6fb99de54ca82abe5137ac700ebf375f
isuke <isuke770@gmail.com>
2017-10-31 16:52:33 +0900

    Write _variables.styl

--- template/src/pages/Top.vue
+++ template/src/pages/Top.vue
@@ -33,11 +33,9 @@ export default =
 
 <style lang="stylus">
 .top
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   text-align: center
-  color: #2c3e50
   margin-top: 60px
 
   h1, h2
